b1cd1cb7b6107ccc4259d0480e88650304b5cddf
Mauricio Selfene <m.selfene@gmail.com>
2022-03-23 23:51:19 -0300

    migracion con datos falsos

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,4 +7,9 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
     //
+    public function index(){
+        return view(index, [
+            'posts' => Post::latest()->paginate()
+        ]);
+    }
 }
