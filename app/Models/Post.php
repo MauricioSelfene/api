2141b9bfd55532b5220960eb4957993c57e03551
Mauricio Selfene <m.selfene@gmail.com>
2022-03-25 01:59:54 -0300

    mas iteraciones

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    public function getExcerptAtrribute(){
+
+        return substr($this->content, 0, 120);
+
+    }
+
+    public function getPublishedAtAtrribute(){
+
+        return $this->created_at->format('d/m/Y');
+    
+    }
+
 }
